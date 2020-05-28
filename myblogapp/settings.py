25cf1943c91ff80443d1c12af06c28b8305f5716
Kota Akiba <chibo.inc.lab@gmail.com>
2020-05-28 20:26:39 +0900

    whitenoise

--- myblogapp/settings.py
+++ myblogapp/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whietnoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
