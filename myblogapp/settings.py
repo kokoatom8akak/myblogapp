263a85285ed0e9e2e727bef3b70545936bbc5d2e
Kota Akiba <chibo.inc.lab@gmail.com>
2020-06-24 17:45:03 +0900

    pics

--- myblogapp/settings.py
+++ myblogapp/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/pics/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/pics/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
